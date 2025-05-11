52a1f3a4f6122011f73e5fab9b2c4b6cf8e0e764
Baltunis <dbaltunis@gmail.com>
2025-05-11 17:10:28 +0300

    🚀 Initial full app push: Neighbourhood Pizza v1.0

---
+++ new pizza app the latest one/new pizza app/new pizza app/new_pizza_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_pizza_appApp.swift
+//  new pizza app
+//
+//  Created by Darius Baltunis on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct new_pizza_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ new pizza app the latest one/new pizza app/new pizza appTests/new_pizza_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  new_pizza_appTests.swift
+//  new pizza appTests
+//
+//  Created by Darius Baltunis on 5/11/25.
+//
+
+import Testing
+@testable import new_pizza_app
+
+struct new_pizza_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
